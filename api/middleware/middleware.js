39b63ebc5aa2002bae5574ea14825f7d03ea73ef
Ashish Desai <desai.ashish76@gmail.com>
2024-05-07 17:25:40 -0400

    removed comments and logs

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,8 +1,6 @@
 const Users = require('../users/users-model')
-const Posts = require('../posts/posts-model')
 
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.orignalUrl
@@ -11,7 +9,6 @@ function logger(req, res, next) {
 }
 
 async function validateUserId (req, res, next) {
-  // DO YOUR MAGIC
   const { id } = req.params
   await Users.getById(id)
     .then(user => {
@@ -32,25 +29,19 @@ async function validateUserId (req, res, next) {
 }
 
 async function validateUser(req, res, next) {
-  // DO YOUR MAGIC
   const { name } = req.body
-  // why not required for post?
-  // await Users.getUserPosts(name) 
-      if (!name || !name.trim()) {
-        res.status(400).json({
-          message: "missing required name field"
-        })
-      } else {
-        req.name = name.trim()
-        next()
-      }
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
   const { text } = req.body
-  // why not required for post?
-  // await Users.getUserPosts(name) 
   if (!text || !text.trim()) {
     res.status(400).json({
       message: "missing required text field"
@@ -61,7 +52,6 @@ function validatePost(req, res, next) {
   }
 }
 
-// do not forget to expose these functions to other modules
 module.exports = {
   logger,
   validateUserId,
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const {validateUserId, validateUser, validatePost} = require('../middleware/middleware')
 
-// You will need `users-model.js` and `posts-model.js` both
 const Users = require('./users-model')
 const Posts = require('../posts/posts-model')
 
-// The middleware functions also need to be required
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
   Users.get()
     .then(users => {
       res.status(200).json(users)
@@ -24,7 +21,6 @@ router.get('/', (req, res) => {
 
 });
 
-//how do I include catch?***
 router.get('/:id', validateUserId, async (req, res) => {
     await res.json(req.user)
 });
@@ -41,9 +37,6 @@ router.post('/', validateUser, async (req, res, next) => {
 });
 
 router.put('/:id', validateUserId, validateUser, async (req, res, next) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
   const { id } = req.params
   const { name } = req.body
   await Users.update(id, {name: name.trim()})
@@ -57,8 +50,6 @@ router.put('/:id', validateUserId, validateUser, async (req, res, next) => {
 });
 
 router.delete('/:id', validateUserId, async (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
   const { id } = req.params
   await Users.remove(id)
     .then(() => {
@@ -67,8 +58,6 @@ router.delete('/:id', validateUserId, async (req, res) => {
 });
 
 router.get('/:id/posts', validateUserId, async (req, res, next) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
   const { id } = req.params
   await Users.getUserPosts(id)
     .then((user) => {
@@ -78,9 +67,6 @@ router.get('/:id/posts', validateUserId, async (req, res, next) => {
 })
 
 router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
   const { id } = req.params
   const { text } = req.body
   await Posts.insert({user_id: id, text: text})
@@ -96,5 +82,5 @@ router.use((err, req, res, next) => { //eslint-disable-line
     message: err.message,
   });
 });
-// do not forget to export the router
+
 module.exports = router
